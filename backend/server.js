d114f607318305b813e8388d1bc3f6956becedb7
Gabriel Grecia <gabriel.lena81@gmail.com>
2025-08-16 08:01:57 -0400

    feat: adiciona rotas e configs de cors

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,27 @@
 const express = require("express")
 const app = express();
+const cors = require('cors');
+// const authRoute = require('routes/authRoute')
+const fileRoutes = require('./routes/filesRoutes')
 const port = 8000;
 
-app.get('/', (req, res) => {
-    res.send("Servidor rodando")
-})
+app.use(cors({
+    origin: [
+        'http://localhost:5173',
+        'http://192.168.100.211:5173',
+        'http://127.0.0.1:5173',
+    ],
+    credentials: true,
+    methods: ['GET', 'POST', 'DELETE', 'PUT'],
+    allowedHeaders: ['Content-Type', 'Content-Disposition', 'Authorization'],
+}));
+
+app.use("/api/v1/auth", (req, res, next) => {
+    res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+    next()
+});
+
+app.use("/api/v1/files", fileRoutes);
 
 app.listen(port, () => {
     console.log("Servidor rodando")

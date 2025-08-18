e6304b659b58b177486d7f203f78e0a30321ee2b
Gabriel Grecia <gabriel.lena81@gmail.com>
2025-08-18 10:55:47 -0400

    feat: adiciona gerenciamento avançado de arquivos
    - Suporte a múltiplos uploads
    - Implementado sistema de renomação de arquivos via modal
    - Listagem de arquivos com tipos, tamanhos e ordenação
    - Suporte a download e deleção de arquivos
    - Adiciona componentes UploadPanel, FileTable, CreateDirectory e Button
    - Melhorias de UX com botões estilizados

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,68 +1,94 @@
 import { useState, useEffect, useRef } from 'react'
 import axios from 'axios';
-import './App.css'
+import UploadPanel from './components/uploadPanel';
+import Modal from "react-modal";
+import styles from './App.module.css'
+import FileTable from './components/fileTable';
+import CreateDirectory from './components/createDirectory';
+import Button from './components/button';
+
+Modal.setAppElement("#root");
 
 interface Files {
   id: number;
   nome: string;
   size: number;
+  type: string;
+}
+
+interface UploadPanel {
+  title: string;
+  subtitle: string;
+  progress: number;
+}
+
+interface selectedFile {
+  id: number,
+  name: string,
 }
 
 function App() {
+  const [file, setFile] = useState<UploadPanel | null>(null);
   const [files, setFiles] = useState<Files[]>([]);
+  const [visible, setVisible] = useState(false);
   const inputRef = useRef<HTMLInputElement>(null);
+  const [isOpen, setIsOpen] = useState(false);
+  const [isDirOpen, setIsDirOpen] = useState(false);
+  const [selectedFile, setSelectedFile] = useState<selectedFile | null>({ id: 0, name: '' });
+  const [path, setPath] = useState("");
 
-  const fetchFiles = async () => {
+  const fetchFiles = async (path: string) => {
     try {
-      const res = await fetch("http://localhost:8000/api/v1/files/list");
+      const safePath = path ?? ''
+      const res = await fetch(`http://localhost:8000/api/v1/files/list?dir=${safePath}`);
       const data = await res.json();
-      setFiles(data);
+      setPath(safePath)
+      setFiles(data.arquivos);
     } catch (err) {
       console.error("Erro:", err);
     }
   };
 
   useEffect(() => {
-    fetchFiles();
-  }, []);
+    fetchFiles(path);
+  }, [path]);
 
   const handleButtonClick = () => {
     inputRef.current?.click();
   };
 
-  const handleDownload = async (id: number) => {
-    const res = await fetch(`http://localhost:8000/api/v1/files/download/${id}`);
-    const disposition = res.headers.get("Content-Disposition");
-    let filename = `arquivo_${id}`;
-    if (disposition && disposition.includes("filename=")) {
-      filename = disposition.split("filename=")[1].replace(/"/g, "");
-    }
-    const blob = await res.blob();
-    const url = window.URL.createObjectURL(blob);
-    const a = document.createElement('a');
-    a.href = url;
-    a.download = filename;
-    document.body.appendChild(a);
-    a.click();
-    a.remove();
-    window.URL.revokeObjectURL(url);
+  const handleDownload = (filename: string, directory: string) => {
+    const url = `http://localhost:8000/api/v1/files/download?folder=${encodeURIComponent(directory)}&file=${encodeURIComponent(filename)}`;
+    window.open(url, "_blank");
   };
+
 
   const handleUpload = async (e: React.ChangeEvent<HTMLInputElement>) => {
     if (!e.target.files || e.target.files.length === 0) return;
-    const file = e.target.files[0];
+    const files = Array.from(e.target.files);
     const formData = new FormData();
 
-    formData.append("file", file);
+    files.forEach(file => formData.append("files", file));
+
+    setFile({
+      title: `Fazendo upload de ${files.length} item(s)`,
+      subtitle: files.map(f => f.name).join(", "),
+      progress: 0,
+    });
+    setVisible(true);
 
     try {
       await axios.post("http://localhost:8000/api/v1/files/upload", formData, {
         onUploadProgress: (progressEvent) => {
-          const percent = Math.round((progressEvent.loaded * 100) / file.size);
-          console.log("Progresso:", percent, "%");
+          const percent = Math.round((progressEvent.loaded * 100) / files.reduce((acc, f) => acc + f.size, 0));
+          setFile(prev => prev ? { ...prev, progress: percent } : prev);
+          console.log('percent: ', percent)
         },
       });
-      fetchFiles()
+      if (file) {
+        setFile({ ...file, title: 'Upload completo' });
+      }
+      fetchFiles(path)
     } catch (error) {
       console.error("Erro no upload:", error);
     }
@@ -70,10 +96,22 @@ function App() {
 
   };
 
+  const handleRename = async (id: number, name: string,) => {
+    console.log(id, name)
+    try {
+      await axios.patch(`http://localhost:8000/api/v1/files/rename/${id}`, {
+        newName: name
+      })
+      fetchFiles(path)
+    } catch (error) {
+      console.error("Erro ao renomear arquivo:", error);
+    }
+  }
+
   const handleDelete = async (id: number) => {
     try {
-      const res = await axios.delete(`http://localhost:8000/api/v1/files/delete/${id}`)
-      fetchFiles()
+      await axios.delete(`http://localhost:8000/api/v1/files/delete/${id}`)
+      fetchFiles(path)
     } catch (error) {
       console.error("Erro ao deletar arquivo:", error);
     }
@@ -81,29 +119,70 @@ function App() {
 
   return (
     <>
-      <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', gap: 10 }}>
-        <h1>Arquivos</h1>
-        <button onClick={handleButtonClick}>Escolher Arquivo</button>
+      <div className={styles.container}>
+        <h1>Driveubas/{path}</h1>
+        <div style={{ display: 'flex', flexDirection: 'row', gap: 12, marginBottom: 24, alignSelf: 'flex-start' }}>
+          <button className={styles.buttonNew} onClick={handleButtonClick}>Upload</button>
+          <button className={styles.buttonNew} onClick={() => setIsDirOpen(true)}>Novo Diretorio</button>
+        </div>
         <input
           type="file"
+          multiple
           ref={inputRef}
           style={{ display: "none" }}
           onChange={handleUpload}
         />
-        {files.map(a => (
-          <div key={a.id} style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
-            <li >
-              {a.nome} ({a.size} bytes)
-            </li>
-            <button style={{ backgroundColor: '#0092ccff', padding: 15, border: 'none', borderRadius: 5 }} onClick={() => { handleDownload(a.id); console.log("downloaded") }}>
-              <span style={{ color: 'white', fontWeight: 'bold' }}>Download</span>
-            </button>
-            <button style={{ backgroundColor: '#e76262ff', padding: 15, border: 'none', borderRadius: 5 }} onClick={() => handleDelete(a.id)} >
-              <span style={{ color: 'white', fontWeight: 'bold' }}>Delete</span>
-            </button>
-          </div>
-        ))}
+        <FileTable
+          files={files}
+          setFiles={setFiles}
+          onDownload={handleDownload}
+          onDelete={handleDelete}
+          onFetch={fetchFiles}
+          onRename={setSelectedFile}
+          setIsOpen={setIsOpen}
+          path={path}
+          setPath={setPath}
+        />
+        {file && (<UploadPanel title={file.title} subtitle={file.subtitle} progress={file.progress} visible={visible} setVisible={setVisible} />)}
       </div>
+      <Modal style={{
+        content: {
+          width: "15%",
+          maxWidth: "500px",
+          margin: "auto",
+          inset: "unset",
+          padding: "24px",
+          borderRadius: "8px"
+        },
+        overlay: {
+          backgroundColor: "rgba(0,0,0,0.6)",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center"
+        }
+      }}
+        isOpen={isOpen} onRequestClose={() => setIsOpen(false)}
+      >
+        <div style={{ display: 'flex', flexDirection: 'column', gap: 12 }}>
+          <p style={{ padding: 0, margin: 0 }}>Renomear</p>
+          <input
+            type="text"
+            value={selectedFile?.name}
+            onChange={(e) => setSelectedFile(prev =>
+              prev ? { ...prev, name: e.target.value } : { id: 0, name: e.target.value }
+            )}
+            autoFocus={true}
+            maxLength={100}
+            spellCheck={false}
+          />
+          <div style={{ display: 'flex', flexDirection: 'row', alignSelf: 'flex-end', gap: 12 }} >
+            <Button text='Cancelar ' variant='cancel' onClick={() => setIsOpen(false)} />
+            <Button text='OK' variant="confirm" onClick={() => { handleRename(selectedFile?.id || 0, selectedFile?.name || ''); setIsOpen(false) }} />
+          </div>
+        </div>
+
+      </Modal>
+      <CreateDirectory isOpen={isDirOpen} setIsOpen={setIsDirOpen} fetchFiles={fetchFiles} />
     </>
   )
 }
